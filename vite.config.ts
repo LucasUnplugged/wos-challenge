7ea5199b47babfb5c84a5fa77dfb7fa50d6fbf54
Lucas Castro <lucas.castro@gmail.com>
2024-05-19 14:36:11 -0400

    fix: Updates to fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 const hash = Math.floor(Math.random() * 90000) + 10000;
 

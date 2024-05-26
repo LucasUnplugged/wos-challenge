1fd925c3d21a8a91b001a82ea09a7f6302188c3c
Lucas Castro <lucas.castro@gmail.com>
2024-05-26 09:22:03 -0400

    chore: Update build files.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-const hash = Math.floor(Math.random() * 90000) + 10000;
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name]` + hash + `.js`,
-        chunkFileNames: `[name]` + hash + `.js`,
-        assetFileNames: `[name]` + hash + `.[ext]`,
-      },
-    },
-  },
 });
